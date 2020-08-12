7245d3fa60abcc6356c1082f91e82af966ecd78a
neda_ <neda@nda.localdomain>
2020-08-12 23:40:50 +0430

    some changes

--- src/container/App.js
+++ src/container/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 // import Messenger from '../components/messenger/messenger'
-import Login from '../components/auth/Login'
+// import Login from '../components/auth/Login'
+import Signup from '../components/auth/Signup'
 
 function App () {
   return (
     <div className='App'>
-      <Login />
+      {/* <Login /> */}
+      <Signup />
       {/* <Messenger /> */}
     </div>
   )
