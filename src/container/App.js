b826fc60244c8136dd7a00b958bf1d1b8fcf3184
neda_ <neda@nda.localdomain>
2020-09-18 12:03:16 +0430

    Routing changes

--- src/container/App.js
+++ src/container/App.js
@@ -11,9 +11,15 @@ function App () {
     <div className='App'>
       <Switch>
         <Route exact path='/heXico' component={Login} />
+<<<<<<< HEAD
         <Route path='/heXico/Signup' component={Signup} />
         <Route path='/heXico/messenger' component={Messenger} />
         <Route path='/heXico/profile' component={Profile} />
+=======
+        <Route path='/Signup' component={Signup} />
+        <Route path='/heXico/messenger' component={Messenger} />
+        <Route path='/profile' component={Profile} />
+>>>>>>> f812c8fdfcb8639f918b91e4fa9b48aae423afe2
       </Switch>
     </div>
   )
