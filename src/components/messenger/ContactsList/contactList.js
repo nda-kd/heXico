2ade8526f78b244ffc51ae53d85aa9f2dbfc83f4
Neda Khorshid <nda.khorshid@gmail.com>
2021-04-15 15:59:19 +0430

    Some minor changes

--- src/components/messenger/ContactsList/contactList.js
+++ src/components/messenger/ContactsList/contactList.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import Me from '../Me/me'
-import searchIcon from '../../../assets/search.png'
-import './contact-list-style.scss'
-import USERS from '../../../user'
-import Contacts from '../Contacts/contacts'
+import React from "react";
+import Me from "../Me/me";
+import searchIcon from "../../../assets/search.png";
+import "./contact-list-style.scss";
+import USERS from "../../../user";
+import Contacts from "../Contacts/contacts";
 
 class ContactList extends React.Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
 
     this.state = {
-      users: USERS
-    }
+      users: USERS,
+    };
   }
 
-  render () {
-    console.log('USERS', this.state.searchField)
+  render() {
+    console.log("USERS", this.state.searchField);
     return (
-      <div className='contacts-me-search-wraps'>
+      <div className="contacts-me-search-wraps">
         <Me />
-        <div className='contacts-search-wrap'>
-          <img src={searchIcon} alt='Search Icon' />
-          <input
-            type='text'
-            name='search'
-            placeholder='Search...'
-          />
+        <div className="contacts-search-wrap">
+          <img src={searchIcon} alt="Search Icon" />
+          <input type="text" name="search" placeholder="Search..." />
         </div>
-        <div className='contact-lists'>
-          {
-          this.state.users.map((user, userID) => (
+        <div className="contact-lists">
+          {this.state.users.map((user, userID) => (
             <Contacts
               key={userID}
               id={user.id}
@@ -39,12 +34,11 @@ class ContactList extends React.Component {
               avatar={user.avatar}
               conversationList={user.conversationList}
             />
-          ))
-        }
+          ))}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default ContactList
+export default ContactList;
